e0ca04b178f49669914c105d069ad2931d5fbc1c
Shilov-sl <61949961+Shilov-sl@users.noreply.github.com>
2020-08-17 20:15:07 +0300

    Добавьте файлы проекта.

---
+++ MyFirstCPPprogram/MyFirstCPPprogram.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int cinNumber(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int a, b;
+	std::cout << "Enter first number: " << std::endl;
+	std::cin >> a;
+	std::cout << "Enter second number: " << std::endl;
+	std::cin >> b;
+	std::cout << "Sum " << a << " + " << b << " = " << cinNumber(a, b) << std::endl;
+	return 0;
+}
