c506f8e3050bcf4506fc9550d72c5c571e347a52
Shilov-sl <61949961+Shilov-sl@users.noreply.github.com>
2020-08-19 17:07:47 +0300

    Code refactoring

---
+++ MyFirstCPPprogram/main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int getUserInput()
+{
+	std::cout << "Please enter an integer:";
+	int value;
+	std::cin >> value;
+	return value;
+}
+
+char getMathematicalOperation()
+{
+	std::cout << "Please enter which operator you want:";
+	char op;
+	std::cin >> op;
+	return op;
+}
+
+int calculateResult(int x, char op, int y)
+{
+	switch (op)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	case '%':
+		return x % y;
+	}
+	return -1;
+}
+
+void printResult(int result)
+{
+	std::cout << "Your result is: " << result << std::endl;
+}
+
+int main()
+{
+	int input1 = getUserInput();
+	int operation = getMathematicalOperation();
+	int input2 = getUserInput();
+	int result = calculateResult(input1, operation, input2);
+	printResult(result);
+	return 0;
+}
